c198d04e7372f2fdfd9edda2b4ff0ef34d48472f
Anton Lehmann <anton.lehmann@student.htw-berlin.de>
2025-11-10 15:27:31 +0100

    connected values to tabpauseUI

--- Assets/Scripts/UI/PlayerTabMenuController.cs
+++ Assets/Scripts/UI/PlayerTabMenuController.cs
@@ -4,13 +4,53 @@ using UnityEngine.UIElements;
 
 public class PlayerTabMenuController : MonoBehaviour
 {
-    [SerializeField] private UIDocument tabMenu; 
+    [SerializeField] private UIDocument tabMenu;
+    [SerializeField] private PlayerStatsManager stats;
+    private Label
+        labelMaxHealth,
+        labelMaxHealthP,
+        labelAttackDamage,
+        labelAttackDamageP,
+        labelAttackSpeed,
+        labelAttackSpeedP,
+        labelBulletSpeed,
+        labelBulletDistance,
+        labelBulletSpread,
+        labelDefense;
+    private Label
+        labelMoveSpeed,
+        labelMoveSpeedP,
+        labelDashDistance,
+        labelDashCooldown,
+        labelDashCooldownP,
+        labelDashes;
     private bool isTabHeld = false;
     private Coroutine timeScaleCoroutine;
 
     void Start()
     {
-        tabMenu.rootVisualElement.style.display = DisplayStyle.None;
+        var root = tabMenu.rootVisualElement;
+
+        labelMaxHealth = root.Q<Label>("labelMaxHealth");
+        labelMaxHealthP = root.Q<Label>("labelMaxHealthP");
+        labelAttackDamage = root.Q<Label>("labelAttackDamage");
+        labelAttackDamageP = root.Q<Label>("labelAttackDamageP");
+        labelAttackSpeed = root.Q<Label>("labelAttackSpeed");
+        labelAttackSpeedP = root.Q<Label>("labelAttackSpeedP");
+        labelBulletSpeed = root.Q<Label>("labelBulletSpeed");
+        labelBulletDistance = root.Q<Label>("labelBulletDistance");
+        labelBulletSpread = root.Q<Label>("labelBulletSpread");
+        labelDefense = root.Q<Label>("labelDefense");
+
+        labelMoveSpeed = root.Q<Label>("labelMoveSpeed");
+        labelMoveSpeedP = root.Q<Label>("labelMoveSpeedP");
+        labelDashDistance = root.Q<Label>("labelDashDistance");
+        labelDashCooldown = root.Q<Label>("labelDashCooldown");
+        labelDashCooldownP = root.Q<Label>("labelDashCooldownP");
+        labelDashes = root.Q<Label>("labelDashes");
+
+        root.style.display = DisplayStyle.None;
+        AssignValues();
     }
     void Update()
     {
@@ -29,11 +69,12 @@ public class PlayerTabMenuController : MonoBehaviour
 
     private void OpenTabMenu()
     {
+        
         if(GameState.inShop || GameState.inPauseMenu)
         {
             return;
-        }   
-
+        }
+        AssignValues();
         GameState.inTabPauseMenu = true;
         isTabHeld = true;
 
@@ -58,6 +99,28 @@ public class PlayerTabMenuController : MonoBehaviour
         timeScaleCoroutine = StartCoroutine(AdjustTimeScale(Time.timeScale, 1f));
     }
 
+    private void AssignValues()
+    {
+        labelMaxHealth.text = $"{Mathf.RoundToInt(stats.GetStat(StatType.MaxHealth) * 10) / 10}";
+        labelAttackDamage.text = $"{Mathf.Round(stats.GetStat(StatType.AttackDamage) * 10) / 10}";
+        labelAttackSpeed.text = $"{Mathf.Round(stats.GetStat(StatType.AttackSpeed) * 10) / 10}";
+        labelBulletSpread.text = $"{Mathf.Round(stats.GetStat(StatType.BulletSpread) * 10) / 10}";
+        labelBulletDistance.text = $"{Mathf.Round(stats.GetStat(StatType.BulletDistance) * 10) / 10}";
+        labelBulletSpeed.text = $"{Mathf.Round(stats.GetStat(StatType.BulletSpeed) * 10) / 10}";
+        labelDefense.text = $"{Mathf.RoundToInt(stats.GetStat(StatType.Defense) * 10) / 10}";
+        labelMoveSpeed.text = $"{Mathf.Round(stats.GetStat(StatType.MoveSpeed) * 10) / 10}";
+        labelDashDistance.text = $"{Mathf.Round(stats.GetStat(StatType.DashDistance) * 10) / 10}";
+        labelDashCooldown.text = $"{Mathf.Round(stats.GetStat(StatType.DashCooldown) * 10) / 10}";
+        labelDashes.text = $"{Mathf.RoundToInt(stats.GetStat(StatType.Dashes) * 10) / 10}";
+
+        labelMaxHealthP.text = $"{Mathf.Round(stats.GetStatMulti(StatMulti.MaxHealthPercent) * 20) / 20}";
+        labelAttackDamageP.text = $"{Mathf.Round(stats.GetStatMulti(StatMulti.AttackPercent) * 20) / 20}";
+        labelAttackSpeedP.text = $"{Mathf.Round(stats.GetStatMulti(StatMulti.AttackSpeedPercent) * 20) / 20}";
+        labelMoveSpeedP.text = $"{Mathf.Round(stats.GetStatMulti(StatMulti.MovePercent) * 20) / 20}";
+        labelDashCooldownP.text = $"{Mathf.Round(stats.GetStatMulti(StatMulti.DashCooldownPercent) * 20) / 20}";
+
+    }
+
     private IEnumerator AdjustTimeScale(float start, float end)
     {
         float t = 0f;
@@ -72,5 +135,7 @@ public class PlayerTabMenuController : MonoBehaviour
         Time.timeScale = end;
         Time.fixedDeltaTime = 0.02f * Time.timeScale;
     }
+
+
 
 }
